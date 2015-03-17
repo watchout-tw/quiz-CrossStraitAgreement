56b0ee1b17af5ec9bfbfa432739f8c2ce4f841c4
Yuan Hsiang Cheng <lyforever62@hotmail.com>
2015-03-17 16:44:31 +0800

    Fix wrong condition expression

--- server.js
+++ server.js
@@ -13,16 +13,16 @@ app.get('/', function (req, res) {
   var html = fs.readFileSync(__dirname + '/index.html', { encoding:'utf8' });
 
   return request.get('https://qa10.firebaseio.com/totalVotesCount.json', function(err, response, body){
-    var votes = JSON.parse(body).votes;
-    if( 0 <= votes < 299) {
+    var votes = Number(JSON.parse(body).votes);
+    if( 0 <= votes && votes < 299) {
       diff = 300 - votes;
-    } else if ( 300 <= votes < 499 ){
+    } else if ( 300 <= votes && votes < 499 ){
       diff = 500 - votes;
       currentLevel = 2;
-    } else if ( 500 <= votes < 699 ){
+    } else if ( 500 <= votes && votes < 699 ){
       diff = 700 - votes;
       currentLevel = 3;
-    } else if ( 700 <= votes < 887 ){
+    } else if ( 700 <= votes && votes < 887 ){
       diff = 888 - votes;
       currentLevel = 4;
     } else {
@@ -30,7 +30,7 @@ app.get('/', function (req, res) {
     }
 
     if(currentLevel < 5) {
-      html = html.replace(/\{\{description\}\}/, "人民來立法，千人揪副本！還差 " + parseInt(diff, 10) + " 人就能進化到Lv. " + (currentLevel + 1) + "！");
+      html = html.replace(/\{\{description\}\}/, "人民來立法，千人揪副本！還差 " + diff + " 人就能進化到Lv. " + (currentLevel + 1) + "！");
     } else {
       html = html.replace(/\{\{description\}\}/, "人民來立法，千人揪副本！已經成功達到Lv. 5！");
     }
